db1932d947b06c6bd366cf1803d95bac5422e935
Pavel Alexeev <pasha.alexeev@gmail.com>
2012-09-11 12:50:13 +0400

    UIColorFromRGBWithAlpha

--- Macros.h
+++ Macros.h
@@ -39,6 +39,7 @@ static inline NSString *StringFromObject(id object) {
 
 // example usage: UIColorFromHex(0x9daa76)
 #define UIColorFromHex(hexValue) [UIColor colorWithRed:((float)((hexValue & 0xFF0000) >> 16))/255.0 green:((float)((hexValue & 0xFF00) >> 8))/255.0 blue:((float)(hexValue & 0xFF))/255.0 alpha:1.0]
+#define UIColorFromHexWithAlpha(rgbValue,a) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:a]
 #define UIColorFromRGBA(r,g,b,a) [UIColor colorWithRed:r/255.0 green:g/255.0 blue:b/255.0 alpha:a]
 #define UIColorFromRGB(r,g,b) UIColorFromRGBA(r,g,b,1.0)
 
