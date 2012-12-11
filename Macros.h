cb0d8c1f693b03625d3d7d35bcc1d1b48671f683
eugenedymov <dymov@me.com>
2012-12-11 11:33:53 +0400

    UIColorRandomColor macros added.

--- Macros.h
+++ Macros.h
@@ -42,6 +42,7 @@ static inline NSString *StringFromObject(id object) {
 #define UIColorFromHexWithAlpha(rgbValue,a) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:a]
 #define UIColorFromRGBA(r,g,b,a) [UIColor colorWithRed:r/255.0 green:g/255.0 blue:b/255.0 alpha:a]
 #define UIColorFromRGB(r,g,b) UIColorFromRGBA(r,g,b,1.0)
+#define UIColorRandomColor [UIColor colorWithHue:( arc4random() % 256 / 256.0 ) saturation:( arc4random() % 128 / 256.0 ) + 0.5 brightness:( arc4random() % 128 / 256.0 ) + 0.5 alpha:( arc4random() % 256 / 256.0 ) + 0.1]
 
 #define UIColorRed(color) CGColorGetComponents(color)[0]
 #define UIColorGreen(color) CGColorGetComponents(color)[1]
